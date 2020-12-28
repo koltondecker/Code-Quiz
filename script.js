a13a6143fb7697cc41bdf1240a0205e7a6c0b68c
Kolton Decker <deckerman25@gmail.com>
2020-12-28 09:41:51 -0600

    Added time penalty of 10 seconds each time a wrong answer is chosen. The final time is then displayed at the end on the 'All Done' page. Correct answers do not invoke a penalty on the time.

--- script.js
+++ script.js
@@ -8,27 +8,50 @@ $("#button-start").on("click", function() {
 function runQuiz() {
     var i = 0;
     var j = 0;
-    var timeSeconds = 10;
+    var timeSeconds = 100;
     var outOfTime;
+    var score = timeSeconds;
     
     interval = setInterval(function() {
         if (timeSeconds > 0) {
-            timeSeconds--;
-            $("#time").text(timeSeconds);
+            score--;
+            $("#time").text(score);
         }
         else {
             $("#time").text("0");
             clearInterval(interval);
             allDone(j);
         }
-
+        return score;
       }, 1000);
     
     $(".answer-button").on("click", goToNextQuestion);
 
+    // $(".answer-button").on("click", function() {
+    //     if ($(this).hasClass("correct-answer")) {
+    //         //todo: need to add code that puts 'Correct' on the screen after a correct answer is chosen.
+    //     }
+    //     else {
+    //         //todo: need to add code that puts 'Wrong' on the screen after a wrong answer is chosen.
+    //         score -= 10;
+    //         return score;
+    //     }
+    // });
+
     function goToNextQuestion() {
         i++;
         j = i + 1;
+
+        if ($(this).hasClass("correct-answer")) {
+            //todo: need to add code that puts 'Correct' on the screen after a correct answer is chosen.
+        }
+        else {
+            //todo: need to add code that puts 'Wrong' on the screen after a wrong answer is chosen.
+            score -= 10;
+            $("#time").text(score);
+
+        }
+
         if (i < 3) {
             $("#question-" + i).addClass("hidden");
             $("#question-" + j).removeClass("hidden");
@@ -38,7 +61,8 @@ function runQuiz() {
             clearInterval(interval);
             allDone(j);
         }
-        return j;
+
+        return j, score;
     }
 
     function allDone(j) {
