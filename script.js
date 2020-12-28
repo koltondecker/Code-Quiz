66cc2c303252698b4c122274bfbfe0227850982a
Kolton Decker <deckerman25@gmail.com>
2020-12-27 22:18:58 -0600

    Added functionality to all buttons that then load the appropriate content/pages from the click.

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
+var i = 0;
+
 $("#button-start").on("click", function() {
     $("#front-page").addClass("hidden");
     $("#question-1").removeClass("hidden");
+});
+
+$(".answer-button").on("click", goToNextQuestion);
+
+function goToNextQuestion() {
+    i++;
+    var j = i + 1;
+    if (i < 3) {
+        $("#question-" + i).addClass("hidden");
+        $("#question-" + j).removeClass("hidden");
+    }
+    else {
+        $("#question-" + i).addClass("hidden");
+        $("#all-done").removeClass("hidden");
+    }
+}
+
+$("#submit-initials").on("click", function submitHighscore() {
+    window.location.href = "highscores.html";
 });
