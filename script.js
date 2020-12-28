114d412ad13e7c0a2c41450249653710f14de2ba
Kolton Decker <deckerman25@gmail.com>
2020-12-27 22:54:37 -0600

    Added timer that counts down to zero once quiz is started, stops at current time if quiz is completed prior to reaching zero. Does not have time penalty function yet.

--- script.js
+++ script.js
@@ -1,25 +1,57 @@
-var i = 0;
 
 $("#button-start").on("click", function() {
     $("#front-page").addClass("hidden");
     $("#question-1").removeClass("hidden");
+    runQuiz();
 });
 
-$(".answer-button").on("click", goToNextQuestion);
+function runQuiz() {
+    var i = 0;
+    var j = 0;
+    var timeSeconds = 10;
+    var outOfTime;
+    
+    interval = setInterval(function() {
+        if (timeSeconds > 0) {
+            timeSeconds--;
+            $("#time").text(timeSeconds);
+        }
+        else {
+            $("#time").text("0");
+            clearInterval(interval);
+            allDone(j);
+        }
 
-function goToNextQuestion() {
-    i++;
-    var j = i + 1;
-    if (i < 3) {
-        $("#question-" + i).addClass("hidden");
-        $("#question-" + j).removeClass("hidden");
+      }, 1000);
+    
+    $(".answer-button").on("click", goToNextQuestion);
+
+    function goToNextQuestion() {
+        i++;
+        j = i + 1;
+        if (i < 3) {
+            $("#question-" + i).addClass("hidden");
+            $("#question-" + j).removeClass("hidden");
+        }
+        else {
+            $("#question-" + i).addClass("hidden");
+            clearInterval(interval);
+            allDone(j);
+        }
+        return j;
     }
-    else {
-        $("#question-" + i).addClass("hidden");
+
+    function allDone(j) {
+        if (j === 0) {
+            $("#question-1").addClass("hidden");
+        }
+        else {
+            $("#question-" + j).addClass("hidden");
+        }
         $("#all-done").removeClass("hidden");
-    }
-}
+    } 
 
-$("#submit-initials").on("click", function submitHighscore() {
-    window.location.href = "highscores.html";
-});
+    $("#submit-initials").on("click", function submitHighscore() {
+        window.location.href = "highscores.html";
+    });
+}
