7a4733b28f659ca496d299d501fd1017dc1ffe00
Kolton Decker <deckerman25@gmail.com>
2021-01-04 20:37:19 -0600

    Added comments to javascript file.

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,11 @@
-
+//On click function that hides start page elements, and shows question 1 while starting our timer function.
 $("#button-start").on("click", function() {
     $("#front-page").addClass("hidden");
     $("#question-1").removeClass("hidden");
     startTimer();
 });
 
+//Declaring and intializing variables.
 var count = 0;
 var j = 0;
 var timeSeconds = 100;
@@ -18,6 +19,7 @@ var newHighScore = {
 var initials = "";
 var newHighScore;
 
+//Timer function that is based on the initalized timeSeconds variable of 100 and runs every 1 second.
 function startTimer() {
     interval = setInterval(function() {
     if (timeSeconds > 0) {
@@ -33,9 +35,10 @@ function startTimer() {
     }, 1000);
 }
 
-
+//Every time an answer button is clicked, the next question function is called to display the next question in our html.
 $(".answer-button").on("click", goToNextQuestion);
 
+//Next question function uses count variables to check which question we are on and which one it is to display next based on id values from html file.
 function goToNextQuestion() {
     count++;
     j = count + 1;
@@ -43,6 +46,7 @@ function goToNextQuestion() {
     if ($(this).hasClass("correct-answer")) {
         $("#answer-correctness-text").text("Correct!");
     }
+    //Subtracts 10 from score and time with wrong answers.
     else {
         score -= 10;
         $("#time").text(score);
@@ -54,6 +58,7 @@ function goToNextQuestion() {
         $("#question-" + count).addClass("hidden");
         $("#question-" + j).removeClass("hidden");
     }
+    //After we are out of new questions, it then stops the time and runs the allDone function.
     else {
         $("#question-" + count).addClass("hidden");
         clearInterval(interval);
@@ -63,6 +68,7 @@ function goToNextQuestion() {
     return j, score;
 }
 
+//Displays our All Done page with final score and ability to submit intials to the highscores page.
 function allDone(j) {
     if (j === 0) {
         $("#question-1").addClass("hidden");
@@ -73,7 +79,7 @@ function allDone(j) {
     $("#all-done").removeClass("hidden");
     $("#final-score").text(score);
 } 
-
+//submit button on click function stores the score and initials into our 'newHighScore' object which will eventually be pushed to our local storage array.
 $("#submit-initials").on("click", function submitHighscore() {
     initials = document.getElementById("InitialsInput").value;
     if (initials === "") {
@@ -82,38 +88,38 @@ $("#submit-initials").on("click", function submitHighscore() {
     }
     newHighScore.initials = initials;
     newHighScore.score = score;
-    // window.location.href = "https://koltondecker.github.io/Code-Quiz/html/highscores.html";
-    window.location.pathname = "/Users/koltondecker/Bootcamp-Homework/Code-Quiz/html/highscores.html"
+    //Changes to our highscores.html file when submit button is clicked.
+    window.location.href = "https://koltondecker.github.io/Code-Quiz/html/highscores.html";
+    //Runs our function to populate the highscores to our highscores page table.
     populateHighscores(initials, newHighScore);
     return initials, newHighScore.initials, newHighScore.score;
 });
-
-// if (window.location.href === "https://koltondecker.github.io/Code-Quiz/html/highscores.html") {
-//     populateHighscores();
-// }
-
-if (window.location.pathname === "/Users/koltondecker/Bootcamp-Homework/Code-Quiz/html/highscores.html") {
+//If statement will run our populate highscores function if user ever wants to jump straight to the highscores page and not take the quiz.
+if (window.location.href === "https://koltondecker.github.io/Code-Quiz/html/highscores.html") {
     populateHighscores();
 }
 
+//Populates highscores table from our local storage and new highscore object from quiz.
 function populateHighscores() {
     currentHighscores = JSON.parse(localStorage.getItem("highscore"));
-
+    //Checks if local storage has any values.
     if(currentHighscores !== null) {
         highscoreArray = currentHighscores;
     }
-
+    //
     if(newHighScore.initials !== "" && newHighScore.score !== undefined) {
         if(currentHighscores !== null) {
+            //Checks if initials already exist in highscores so we can update the score associated with those initials rather than create another element with the same initials.
             for(i = 0; i < highscoreArray.length; i++) {
                 if(highscoreArray[i].initials === newHighScore.initials) {
                     highscoreArray[i].score = score;
                 }
-            }    
+            }
+            //If initials don't exist in the array, then we will push the new score. 
             if(highscoreArray.some(e => e.initials === newHighScore.initials) === false) {
                 highscoreArray.push(newHighScore);
             }
-
+            // Sort function sorts highscore array into descending order. 
             highscoreArray.sort(function(a, b) {
                 return parseFloat(b.score - a.score);
             });
@@ -123,13 +129,14 @@ function populateHighscores() {
         }
     }
     localStorage.setItem("highscore", JSON.stringify(highscoreArray));
-
+    //For loop appends table elements to page for highscores table.
     for (i = 0; i < highscoreArray.length; i++) {
         var rank = i + 1;
         $("#table-body").append("<tr> <th scope='row'>" + rank + "</th> <td>" + highscoreArray[i].initials + "</td> <td>" + highscoreArray[i].score + "</td> </tr>");
     }
 } 
 
+//Button that removes highscore array from local storage and clears all text content from our table body element so there will be no more highscores displayed.
 $("#button-clear-scores").on("click", function(event) {
     localStorage.removeItem("highscore");
     var tableBody = document.getElementById("#table-body");
